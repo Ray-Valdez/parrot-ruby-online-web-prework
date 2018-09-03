593832087b55a7967f1552ca63160f16db6aab95
ray_valdez@hotmail.co.uk <ray_valdez@hotmail.co.uk>
2018-09-03 16:10:49 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(says = "Squawk!")
-  puts says
-  return "Pretty Bird!"
+  puts parrot("Pretty Bird!")
+  return says
 end
